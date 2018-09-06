cd1515919bfc0bd081abe23b0dc6ec952d737890
Jerry Wu <wuj828@gmail.com>
2018-09-06 11:23:14 -0700

    Added status component to be rendered on index.jsx

---
+++ client/status.jsx
@@ -0,0 +1,24 @@
+const Status = props => {
+  return (
+    <div className="status-box">
+      <progress value="772" max="8000"></progress><br />
+      <br />
+      <span id="funds-raised">$7,720</span><br />
+      <span className="project-stats-text">pledged as of $20,000 goal</span><br />
+      <br />
+      <span className="project-stats">1,423</span><br />
+      <span className="project-stats-text">backers</span><br />
+      <br />
+      <span className="project-stats">21</span><br />
+      <span className="project-stats-text">days to go</span><br />
+      <br />
+      <button id="back-project">Back this project</button><br />
+      <button id="follow-project"><span class="glyphicon glyphicon-bookmark"></span> Follow Project</button><i class="fab fa-facebook"></i><i class="fab fa-twitter"></i><i class="fas fa-envelope"></i><i class="fas fa-code"></i><br />
+      <br />
+      <p id="project-term"><u>All or nothing.</u> This project will only be funded if it reaches its goal by Thu, September 27 2018 9:56 AM PDT.</p><br />
+    </div>
+  )
+}
+
+
+export default Status
